aed23a2c38f810d5af862d2772fdeed0758f6146
Qualcommes <qualcommes122@gmail.com>
2026-04-16 23:58:07 +0700

    eighth step

---
+++ brain_games/progression_cli.py
@@ -0,0 +1,31 @@
+import random
+
+
+def get_progression(first: int, step: int, n: int) -> list:
+    stack = []
+    position = random.randint(1, n)
+    number = 0
+    progression = ''
+    for i in range(1, n + 1):
+        if i == position:
+            progression += '.. '
+            number += first
+            first += step
+            continue
+        progression += str(first) + ' '
+        first += step
+    stack += [progression, number]
+    return stack
+
+
+def brain_progression_game() -> list:
+    stack = []
+    stack.append('Whut number is missing in the progression?')
+
+    for i in range(1, 4):
+        first = random.randint(1, 20)
+        step = random.randint(1, 15)
+        iterations = random.randint(5, 10)
+        progression = get_progression(first, step, iterations)
+        stack.append([progression[0], progression[1]])
+    return stack
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+from brain_games.game_engine import game
+from brain_games.progression_cli import brain_progression_game
+
+
+def main():
+    stack = brain_progression_game()
+    game(stack)
+
+
+if __name__ == "__main__":
+    main()
